34456c96650b7f8bb0e60fcc90d3431e699e8be8
Alex Demchenko <pilot@pesochin.net>
2016-06-03 19:02:50 +0300

    Update path's

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use opencv::imgproc;
 use opencv::types::{VectorOfint, VectorOfuchar};
 
 fn main() {
-    let mut mat = highgui::imread("/Users/dmitriybelyaev/Desktop/input.jpg", highgui::IMREAD_UNCHANGED).unwrap();
+    let mut mat = highgui::imread("/path/to/source/image/input.jpg", highgui::IMREAD_UNCHANGED).unwrap();
     let buffer = VectorOfint::new();
     let mut dest = cv::Mat::new().unwrap();
     let size = cv::Size { width: 1, height: 1 };
     imgproc::gaussian_blur(&mat, &mut dest, size, 0.0, 0.0, imgproc::BORDER_DEFAULT);
-    let name = format!("/Users/dmitriybelyaev/Desktop/result/txt.jpg");
+    let name = format!("/path/to/result/image/txt.jpg");
     println!("{:?}", highgui::imwrite(&name, &dest, &buffer));            
 }
